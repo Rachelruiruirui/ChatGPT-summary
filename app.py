297ebee4bd32d51178a89b9ff89dc928d1fb64fc
RuiHanRachel <128981444+RuiHanRachel@users.noreply.github.com>
2023-03-28 00:39:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ def app():
         config = yaml.load(file, Loader=SafeLoader)
     
     config['credentials']['usernames']['CICC-Software']['name'] = user_name
+    config['credentials']['usernames']['CICC-Software']['name'] = email
     config['credentials']['usernames']['CICC-Software']['password'] = password
 
     authenticator = stauth.Authenticate(
